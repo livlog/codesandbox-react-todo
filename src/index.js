f411a36e125684a9ba61605b56b41860d4e97b7d
livlog <tomiya.y@gmail.com>
2022-07-06 23:24:30 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import * as ReactDOM from "react-dom";
+import { App } from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
